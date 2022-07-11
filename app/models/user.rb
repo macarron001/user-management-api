de2a2e6eca7f44593983fe71a8f5dec5dcd6e342
macarron001 <rpinoon16@gmail.com>
2022-07-11 15:29:51 +0800

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :role, :inclusion => { :in => ['User', 'Admin', 'Superadmin']}
+  validates :phone_number, presence: true
+
+
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 end
