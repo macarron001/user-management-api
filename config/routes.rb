b64109f7e97382c5d19827dbba0f0d81a6ce082f
macarron001 <rpinoon16@gmail.com>
2022-07-11 17:19:23 +0800

    added routes for current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
+  get '/user/:id', to: 'current_user#show'
+  post '/create', to: 'current_user#create'
+  patch '/user/:id', to: 'current_user#update'
+  delete '/user/:id', to: 'current_user#delete'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
