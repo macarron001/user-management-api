02f7e8701eca6b7f340f2a93bf2b0d85bc119eb7
macarron001 <rpinoon16@gmail.com>
2022-07-11 17:31:25 +0800

    added authentication and admin creation method for superadmin

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,7 +1,8 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin, except: [:show, :index]
-  before_action :get_user, except: [:index, :create]
+  before_action :authenticate_superadmin, only: [:create_admin]
+  before_action :get_user, except: [:index, :create, :create_admin]
 
   def index
     render json: current_user, status: :ok
@@ -42,10 +43,26 @@ class CurrentUserController < ApplicationController
     }, status: :ok
   end
 
+  def create_admin
+    user = User.create!(admin_params)
+
+    render json: json = {
+      status: 201, 
+      message: "Admin registered!",
+      user: user.email,
+      role: user.role,
+      name: user.name
+    }, status: :ok
+  end
+
   private
 
   def user_params 
     params.require(:user).permit(:email, :password, :name, :phone_number)
+  end
+
+  def admin_params 
+    params.require(:user).permit(:email, :password, :name, :phone_number, :role)
   end
 
   def get_user
@@ -57,5 +74,12 @@ class CurrentUserController < ApplicationController
       status: 401, 
       message: 'Unauthorized user.',
     }, status: :unauthorized if current_user.role == 'User'
+  end
+
+  def authenticate_superadmin
+    render json: json = {
+      status: 401, 
+      message: 'Unauthorized user.',
+    }, status: :unauthorized unless current_user.role == 'Superadmin'
   end
 end
