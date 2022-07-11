c2f5b954b90bdd75d22d759cb9287214cbffc9c7
macarron001 <rpinoon16@gmail.com>
2022-07-11 23:49:30 +0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,106 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([{
+  email: "superadmin@user.com",
+  password: "password",
+  role: "Superadmin",
+  name: "Zeus",
+  phone_number: "001-0001"
+},
+{
+  email: "admin1@user.com",
+  password: "password",
+  role: "Admin",
+  name: "Poseidon",
+  phone_number: "001-0002"
+},
+{
+  email: "admin2@user.com",
+  password: "password",
+  role: "Admin",
+  name: "Hades",
+  phone_number: "001-0003"
+},
+{
+  email: "ares@user.com",
+  password: "password",
+  role: "User",
+  name: "Ares",
+  phone_number: "010-0011"
+},
+{
+  email: "hermes@user.com",
+  password: "password",
+  role: "User",
+  name: "Hermes",
+  phone_number: "010-0012"
+},
+{
+  email: "hephaestus@user.com",
+  password: "password",
+  role: "User",
+  name: "Hephaestus",
+  phone_number: "010-0013"
+},
+{
+  email: "dionysus@user.com",
+  password: "password",
+  role: "User",
+  name: "Dionysus",
+  phone_number: "010-0014"
+},
+{
+  email: "aphrodite@user.com",
+  password: "password",
+  role: "User",
+  name: "Aphrodite",
+  phone_number: "010-0015"
+},
+{
+  email: "artemis@user.com",
+  password: "password",
+  role: "User",
+  name: "Artemis",
+  phone_number: "010-0016"
+},
+{
+  email: "hera@user.com",
+  password: "password",
+  role: "User",
+  name: "Hera",
+  phone_number: "010-0017"
+},
+{
+  email: "athena@user.com",
+  password: "password",
+  role: "User",
+  name: "Athena",
+  phone_number: "010-0018"
+},
+{
+  email: "hestia@user.com",
+  password: "password",
+  role: "User",
+  name: "Hestia",
+  phone_number: "010-0019"
+},
+{
+  email: "demeter@user.com",
+  password: "password",
+  role: "User",
+  name: "Demeter",
+  phone_number: "010-0020"
+},
+{
+  email: "persephone@user.com",
+  password: "password",
+  role: "User",
+  name: "Persephone",
+  phone_number: "010-0021"
+},
+{
+  email: "kratos@user.com",
+  password: "password",
+  role: "User",
+  name: "Kratos",
+  phone_number: "010-6066"
+},
+])
