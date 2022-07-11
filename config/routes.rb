73a09f80529399927b51352e962f82aa67e8af31
macarron001 <rpinoon16@gmail.com>
2022-07-11 17:31:51 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
   get '/user/:id', to: 'current_user#show'
   post '/create', to: 'current_user#create'
+  post '/create_admin', to: 'current_user#create_admin'
   patch '/user/:id', to: 'current_user#update'
   delete '/user/:id', to: 'current_user#delete'
 
