c238ba89993be89bbcf2c5b153c9f48602a442e7
macarron001 <rpinoon16@gmail.com>
2022-07-11 23:49:11 +0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
   get '/user/:id', to: 'current_user#show'
   post '/create', to: 'current_user#create'
-  post '/create_admin', to: 'current_user#create_admin'
+  post '/create_super', to: 'current_user#create_super'
   patch '/user/:id', to: 'current_user#update'
   delete '/user/:id', to: 'current_user#delete'
+
+  get '/list', to: 'current_user#user_list'
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
