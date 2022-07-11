9307fc5044e0978ad2927df586dd1073973c1139
macarron001 <rpinoon16@gmail.com>
2022-07-11 16:11:02 +0800

    added basic crud tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before(:all) do
+    @user = User.create(email: 'hercules@user.com', password: 'password', name: 'Hercules', role: 'User', phone_number: '010-0030' )
+  end
+
   context 'validating user creation' do
     it 'is not valid without email' do
       user = User.new
@@ -36,6 +40,27 @@ RSpec.describe User, type: :model do
       user.phone_number = nil
 
       expect(user).not_to be_valid
+    end
+  end
+
+  context 'Validating CRUD actions' do
+
+    it 'should allow a user to be created' do
+      expect(@user).to be_valid
+    end
+
+    it 'should allow a user to be read' do
+      expect(User.find_by(email: 'hercules@user.com')).to eq(@user)
+    end
+
+    it 'should allow a user to be updated' do
+      @user.update(name: 'Herculez')
+      expect(User.find_by(name: 'Herculez')).to eq(@user)
+    end
+
+    it 'should allow a user to be deleted' do
+      @user.destroy
+      expect(User.count).to eq(0)
     end
   end
 end
